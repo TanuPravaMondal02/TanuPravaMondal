b6a7c230c40b7109534179304b1d12218761539a
Richard Britto <richardbritto97@gmail.com>
2026-01-26 00:39:30 -0500

    Fix V2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TanuPravaMondal/",
+  base: "/TanuPravaMondal", // This MUST match your repo name exactly
+})
